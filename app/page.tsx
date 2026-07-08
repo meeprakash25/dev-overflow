e1bb3c13052628004fac7cf1b874272819090dc7
Prakash Poudel <mee.prakash25@gmail.com>
2026-07-09 01:38:44 +0545

    project setup, tailwind theme, logo and assets

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image"
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="h1-bold">Hello World</h1>
+      <h1 className="text-4xl font-space-grotesk">Hello World</h1>
+    </div>
+  )
+}
